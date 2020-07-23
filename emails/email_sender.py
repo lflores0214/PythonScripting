951eaf74afc3786e32d539c010701e5fad7931c8
Luis Flores <lflores02142@gmail.com>
2020-07-23 11:30:21 -0700

    built a very simple email sender

--- emails/email_sender.py
+++ emails/email_sender.py
@@ -1,2 +1,20 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+
+email = EmailMessage()
+email['from'] = 'Luis Flores'
+email['to'] = 'Lflores02142@gmail.com'
+email['subject'] = "There are sexy singles in your area"
+
+email.set_content(html.substitute({"name": "Fry"}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('johndoesit92@gmail.com', 'C3darW00d30')
+    smtp.send_message(email)
+    print('all good!')
