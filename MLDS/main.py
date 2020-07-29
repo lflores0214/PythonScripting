d9a388a6ce14db5447f5e6f5427db21c94abc87d
Luis Flores <lflores02142@gmail.com>
2020-07-28 17:33:06 -0700

    added fifa_data visualization and iris data models and predictions

---
+++ MLDS/main.py
@@ -0,0 +1,8 @@
+# What does a machine learning expert do
+
+#1 - Import the data
+#2 - Clean the data
+#3 - Split the data. Training Set/Test Set
+#4 - Create a Model
+#5 - Check the output
+#6 - Improve
